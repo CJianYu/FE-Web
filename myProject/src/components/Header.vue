c20d118f4f7a7297abffd5a83534dad330eadd67
CJianYu <externalanswer@gmail.com>
2017-05-16 17:51:29 +0800

    a small demo

---
+++ myProject/src/components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h3>Quotes Added</h3>
+    <div class="progress">
+      <div class="progress-bar" role="progressbar"
+      aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" :style="{ width: (quoteCount/maxQuote) * 100+'%'}">
+         {{quoteCount}}/{{maxQuote}}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default{
+      props:["quoteCount","maxQuote"]
+  }
+</script>
---
+++ myProject/src/components/QuoteGrid.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="row">
+     <app-quote v-for="(quote,index) in quotes" @click.native="deleteQuote(index)">
+       {{ quote }}
+     </app-quote>
+  </div>
+</template>
+
+<script>
+  import Quote from './Quotes.vue';
+  export default{
+      props:['quotes'],
+      methods:{
+        deleteQuote:function(index){
+            this.$emit("quoteDeleted",index);
+        }
+      },
+      components:{
+          appQuote:Quote
+      }
+  }
+</script>
